60871e789323b527913eeef59dc059a7e8a353b1
Phoebe Harshitha <phoebeharshitha2319@gmail.com>
2026-06-08 17:25:29 +0530

    fix: update to gemini-2.0-flash

--- Backend/src/services/llm.service.js
+++ Backend/src/services/llm.service.js
@@ -5,8 +5,7 @@ export const analyzeCode = async (code) => {
   const prompt = reviewPrompt(code);
 
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
   const result = await model.generateContent(prompt);
   const text = result.response.text();
 
